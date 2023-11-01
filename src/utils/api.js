41b0c78b3822cc485e44b5fc8d5d663c245ed5ae
Aarushi <aarushikrishna@gmail.com>
2023-11-01 11:40:31 +0530

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN ='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWM0OGFhOWQ5NmU0NzQ5YTM0ZGUzYzMzYjdjMGE3ZiIsInN1YiI6IjY0Y2EzZDVlZDM3MTk3MDBlMjBhODc5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XZm3tNJlWLMrwmqHqb6c1fRLw1Px_as5WHRUJC0b0Rw';
+const TMDB_TOKEN = process.env.TOKEN;
 
 const headers = {
     Authorization: "bearer " + TMDB_TOKEN,
